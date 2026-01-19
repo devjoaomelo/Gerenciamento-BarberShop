4de309ef30ea2dcca2a038b3586b9a03f9e6d502
devjoaomelo <devjoaomelo@gmail.com>
2026-01-19 14:10:04 -0300

    feat: adiciona classe base Entity com campos de auditoria - Id (Guid), DataCriacao e DataAtualizacao - Método AtualizarDataModificacao para rastrear - Setters privados

---
+++ src/BarberShop.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace BarberShop.Domain.Entities;
+
+public abstract class Entity
+{
+    public Guid Id { get; private set;  }
+    public DateTime DataCriacao { get; private set; }
+    public DateTime? DataAtualizacao { get; private set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        DataCriacao = DateTime.Now;
+    }
+
+    public void AtualizarDataModificacao()
+    {
+        DataAtualizacao = DateTime.Now;
+    }
+}
